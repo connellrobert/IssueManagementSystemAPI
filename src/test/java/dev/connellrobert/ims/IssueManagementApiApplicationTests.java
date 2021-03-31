26a9f8b315fda4b1ff3f01afe84bde10dcd7a362
connellrobert <robert.lel.connell@gmail.com>
2021-03-31 12:59:09 -0500

    adding initial project

---
+++ src/test/java/dev/connellrobert/ims/IssueManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.connellrobert.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
